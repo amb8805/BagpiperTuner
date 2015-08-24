a5e22442b54052ac979ba2c3032852d99aabbadc
Adam Blaine <adamblaine@Adams-iMac.local>
2015-08-24 01:30:10 -0400

    initial

---
+++ bagpipeTuner/bagpipeTuner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bagpipeTuner
+//
+//  Created by Adam Blaine on 8/24/15.
+//  Copyright (c) 2015 8beat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
